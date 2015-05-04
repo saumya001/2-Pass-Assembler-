4043a121161fd91bb9965b1d62a7589e0c040c9c
saumya001 <saumya.s09@gmail.com>
2015-05-05 00:50:25 +0530

    Update and rename README.md to pass1.c

--- pass1.c
+++ pass1.c
@@ -1,5 +1,3 @@
-# 2-Pass-Assembler-
-Code in C  for generating machine code of self designed ISA using 2 pass Assembler Logic which was done as a project for COA ( Computer Organization and Architecture ) 
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
